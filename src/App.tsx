dbe7a86fe71e29a7830e97a5647c8412451f7133
Кирсанов Илья Александрович (4047222) <vtb4047222@region.vtb.ru>
2021-04-18 12:05:24 +0300

    fix: change files

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
+          This is my configuration of React, ESLint, Stylelint and Prettier in Visual Studio Code
+          for React development using Typescript.
         </p>
         <a
           className="App-link"
@@ -19,6 +18,46 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+        </a>
+        <a
+          className="App-link"
+          href="https://create-react-app.dev/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Create React App
+        </a>
+        <a
+          className="App-link"
+          href="https://www.typescriptlang.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Typescript
+        </a>
+        <a
+          className="App-link"
+          href="https://eslint.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn ESLint
+        </a>
+        <a
+          className="App-link"
+          href="https://prettier.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Prettier
+        </a>
+        <a
+          className="App-link"
+          href="https://stylelint.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Stylelint
         </a>
       </header>
     </div>
